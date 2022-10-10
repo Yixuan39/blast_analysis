cfde4cd8eb07d665148a209df03f4d11d79bef57
Yixuan Yang <yyang55@ncsu.edu>
2022-10-10 17:01:58 -0400

    change reference files

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import os
 
 if __name__ == '__main__':
-    reference = './reference_data/cow_reference/GCA_000003055.5_Bos_taurus_UMD_3.1.1_genomic.fna'
+    reference = '../../referenceFiles/GCA_021347905.1_ARS-LIC_NZ_Holstein-Friesian_1_genomic.fna'
+    reference_out = '../reference_data/cow_reference/cow_reference'
     input_folder = 'parchment_data_fasta'
     format = '.fasta'
     files = os.listdir(input_folder)
     names = [file.replace(format, '') for file in files if format in file]
-    output_folder = 'mapping_oneHit'
+    output_folder = 'ref_withY2'
     os.makedirs(output_folder, exist_ok=True)
     os.makedirs(output_folder+'_commands', exist_ok=True)
     with open(output_folder+'_commands/run.sh', 'w') as new:
-        new.write('makeblastdb -in .'+ reference +' -dbtype nucl\n')
+        new.write('makeblastdb -in '+ reference +' -dbtype nucl -out ' + reference_out + '\n')
         for name in names:
-            print()
-            new.write('sbatch -c 8 ' + name + '.sh\n')
+            new.write('sbatch -c 16 ' + name + '.sh\n')
         new.close()
     for name in names:
         with open(output_folder+'_commands/' + name + '.sh', 'w') as new:
             new.write('#!/bin/sh\n')
             new.write('blastn -query ../' + input_folder + '/' + name + format +
-                      ' -db .'+ reference +' -num_threads 8 -out ../' + output_folder
-                      + '/' + name + '.csv -outfmt 6 -task megablast -max_hsps 1 -max_target_seqs 1\n')
+                      ' -db '+ reference_out +' -num_threads 16 -out ../' + output_folder
+                      + '/' + name + '.csv -outfmt 6 -evalue 1000 -max_hsps 1 -max_target_seqs 1\n')
             new.close()
-
